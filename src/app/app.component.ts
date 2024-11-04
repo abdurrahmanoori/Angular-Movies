6424e4b2e8fc6ad9e8b6703a0ad78e7e9841ee4e
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-11-04 21:07:26 +0430

    ng for loop added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DatePipe,UpperCasePipe,CurrencyPipe,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-movies';
+  
+  movies = [{
+    title: "spider man",
+    releaseDate: new Date(),
+    Price:100
+  },{
+  title: "Darkan",
+  releaseDate: new Date("2000-2-2"),
+  Price:500
+  }
+]
+  dublicateNumber(value: number):number{
+    return value*2;
+
+  }
 }
