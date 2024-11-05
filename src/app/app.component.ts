248b8c9ad421a50b76f152406d0472afc7988bb8
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-11-05 14:41:16 +0430

    movie list component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,17 @@
 import { CurrencyPipe, DatePipe, NgFor, NgIf, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MoveisListComponent } from "./moveis/moveis-list/moveis-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage,NgIf],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage, NgIf, MoveisListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  movies?: any[];
-  constructor() {
-    setTimeout(() => {
-      this.movies = [{
-        title: "spider man",
-        releaseDate: new Date(),
-        Price: 100,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Cidade_Maravilhosa.jpg/288px-Cidade_Maravilhosa.jpg"
-      }, {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }
-        , {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }
-        , {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }]
-    }, 3000);
-  }
+  
 
 }
