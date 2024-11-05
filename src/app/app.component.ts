2fb5f51770f403da0697b87023f21936c2e2b01f
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-11-05 07:32:14 +0430

    ng for added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,43 @@
-import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgFor, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe,UpperCasePipe,CurrencyPipe,NgFor],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
-  movies = [{
-    title: "spider man",
-    releaseDate: new Date(),
-    Price:100
-  },{
-  title: "Darkan",
-  releaseDate: new Date("2000-2-2"),
-  Price:500
-  }
-]
-  dublicateNumber(value: number):number{
-    return value*2;
 
+  movies?: any[];
+  constructor() {
+    setTimeout(() => {
+      this.movies = [{
+        title: "spider man",
+        releaseDate: new Date(),
+        Price: 100,
+        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Cidade_Maravilhosa.jpg/288px-Cidade_Maravilhosa.jpg"
+      }, {
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
+      }
+        , {
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
+      }
+        , {
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
+      }]
+    }, 3000);
   }
+
 }
