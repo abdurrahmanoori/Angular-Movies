1c7801d7ddf5ae494855018400bcb114bf41908d
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-11-05 15:22:14 +0430

    image changed and also data passed to movies two obj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,35 @@ import { MoveisListComponent } from "./moveis/moveis-list/moveis-list.component"
 })
 export class AppComponent {
 
-  
+  inTheaterMovies?:any[];
+  upCommingReleaseMovies?: any[];
+
+  constructor() {
+    setTimeout(() => {
+      this.inTheaterMovies = [{
+        title: "spider man",
+        releaseDate: new Date(),
+        Price: 100,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7uOE4y3y6sflKds0y5YGpA-gwMvd91IgUeQ&s"
+      }, {
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh2CpkT8o4B7jMifn0ZmWWhO6gZh_3SnbYUw&s"
+      }]
+      this.upCommingReleaseMovies =[{
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGz90Kj6j-qp_yDBmZ5zHRW9XQJZaYge_cng&s"
+      }
+        , {
+        title: "Darkan",
+        releaseDate: new Date("2000-2-2"),
+        Price: 500,
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlC3J637_Pt9fKW4yjh7MNwCRTm5PJxrXZDw&s"
+      }]
+    }, 3000);
+  }
 
 }
--- src/app/moveis/moveis-list/moveis-list.component.ts
+++ src/app/moveis/moveis-list/moveis-list.component.ts
@@ -1,6 +1,6 @@
 import { CurrencyPipe, DatePipe, NgFor, NgIf, NgOptimizedImage, UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
-
+// import { Component, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-moveis-list',
   standalone: true,
@@ -9,32 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './moveis-list.component.css'
 })
 export class MoveisListComponent {
-  movies?: any[];
-  constructor() {
-    setTimeout(() => {
-      this.movies = [{
-        title: "spider man",
-        releaseDate: new Date(),
-        Price: 100,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Cidade_Maravilhosa.jpg/288px-Cidade_Maravilhosa.jpg"
-      }, {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }
-        , {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }
-        , {
-        title: "Darkan",
-        releaseDate: new Date("2000-2-2"),
-        Price: 500,
-        poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Maracan%C3%A3_2014_g.jpg/1024px-Maracan%C3%A3_2014_g.jpg"
-      }]
-    }, 3000);
-  }
+  @Input({ required: true }) 
+  movies?: any[];  
+  // movies?: any[];
 }
