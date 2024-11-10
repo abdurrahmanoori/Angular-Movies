1878c9b457f58470127c9a7505258bf29aff9081
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-11-10 07:39:36 +0430

    45-new pages created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { IndexGenresComponent } from './index-genres/index-genres.component';
-import { CreateGeresComponent } from './create-geres/create-geres.component';
+import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
+import { CreateGeresComponent } from './genres/create-geres/create-geres.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
